fb6d982cad784643188145fc3562be492192e1a2
Nate Stott <nate.stott@pm.me>
2024-05-30 10:28:12 -0600

    complete mvp

--- course/invaders/src/main.rs
+++ course/invaders/src/main.rs
@@ -80,6 +80,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw & render
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
@@ -87,7 +90,17 @@ fn main() -> Result<(), Box<dyn Error>> {
             drawable.draw(&mut curr_frame);
         }
         let _ = render_tx.send(curr_frame);
-        thread::sleep(Duration::from_micros(1));
+        thread::sleep(Duration::from_millis(1));
+
+        // Win or lose
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop
+        }
     }
 
     // Cleanup
