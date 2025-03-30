38b850d8cd5975d6bb943bad39eb0c9fa6668a09
Nate Stott <nate.stott@pm.me>
2025-03-29 20:17:40 -0400

    move semantics and guessing game

--- rustlings/exercises/06_move_semantics/move_semantics5.rs
+++ rustlings/exercises/06_move_semantics/move_semantics5.rs
@@ -4,21 +4,21 @@
 // removing references (the character `&`).
 
 // Shouldn't take ownership
-fn get_char(data: String) -> char {
-    data.chars().last().unwrap()
+fn get_char(s: &String) -> char {
+    s.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = data.to_uppercase();
+fn string_uppercase(mut s: String) {
+    s = s.to_uppercase();
 
     println!("{data}");
 }
 
 fn main() {
-    let data = "Rust is great!".to_string();
+    let s = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&s);
 
-    string_uppercase(&data);
+    string_uppercase(s);
 }
